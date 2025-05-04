e15fedcc61c52582db2e41952d08de2d38190946
KarthiTech99 <23mca10@americancollege.edu.in>
2025-05-04 13:26:52 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const userRoutes = require('./routes/UserRoutes');
 const crimeReportRoutes = require('./routes/CrimeReportRoutes');
 const searchRoutes = require('./routes/SearchRoutes');
 const faqRoutes = require('./routes/FAQRoutes');
 
-require('dotenv').config();
-
 const app = express();
 
 const allowedOrigins = [
-  'https://crime-frontend-nine.vercel.app/',
+  'https://crime-frontend-nine.vercel.app',
   'http://localhost:5173',
 ];
 
@@ -32,7 +31,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use('/api/user', userRoutes);
 app.use('/api/report', crimeReportRoutes);
